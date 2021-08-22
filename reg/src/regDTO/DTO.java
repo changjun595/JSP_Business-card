60e32dccec79a2556abee59d7e7564462ffa178c
UESR <UESR@DESKTOP-1409J43>
2021-08-22 23:30:42 +0900

    jsp명함관리프로그램 (model1)

---
+++ reg/src/regDTO/DTO.java
@@ -0,0 +1,59 @@
+package regDTO;
+
+public class DTO {
+
+	 	
+	 	private String name;
+		private String position;
+	 	private String company;
+	 	private String tell;
+	 	private String email;
+	 	private String address;
+	 
+	 	
+	 	public void setName(String name) {
+	 		this.name = name; 
+	 	}
+		public void setPosition(String position) {
+	 		this.position = position; 
+	 	}
+	 	public void setCompany(String company) {
+	 		this.company = company; 
+	 	}
+	 
+	 	public void setTell(String tell) {
+	 		this.tell = tell; 
+	 	}
+	 
+	 	public void setEmail(String email) {
+	 		this.email = email; 
+	 	}
+	 
+	 	public void setAddress(String address) {
+	 		this.address = address; 
+	 	}
+	 	
+	 	public String getName() {
+	 		return name; 
+	 	}
+		public String getPosition() {
+	 		return position; 
+	 	}
+	 	public String getCompany() {
+	 		return company; 
+	 	}
+	 
+	 	public String getTell() {
+	 		return tell; 
+	 	}
+	  
+	 	public String getEmail() {
+	 		return email; 
+	 	}
+	 
+	 	public String getAddress() {
+	 		return address; 
+	 	}
+	 	
+	 }
+
